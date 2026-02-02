bd7b178d0b22dda575871c6ba9120a6f0a22e784
Sai Varsha <saivarsha481@gmail.com>
2026-02-02 08:24:50 +0530

    Episode -2

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-{/* <div class="parent">
-    "I'm Varsha Sreenivasulu";
-    <div class="child">
-        <h1>Hi I'm h1 tag</h1>
-        <h2>Hi I'm h2 tag</h2>
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement("div", { class: "parent"} ,
-     [ "I'm Varsha Sreenivasulu" , React.createElement("div", { class: "child"} , 
+const heading = React.createElement("div", { className: "parent"} ,
+     [ "I'm Varsha Sreenivasulu" , React.createElement("div", { className: "child"} , 
      [ React.createElement("h1", {}, "Hi I'm h1 tag") , React.createElement("h2", {}, "Hi I'm h2 tag") ])]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
